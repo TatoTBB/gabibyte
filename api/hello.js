1f2210471bdc11003e0b2de8e6601391363b02c6
TatoTBB <tatohd93@gmail.com>
2024-06-07 07:06:39 -0400

    Updatesito...

--- api/hello.js
+++ api/hello.js
@@ -1,4 +1,3 @@
-export default function handler(request, response) {
-  const { name = 'World' } = request.query;
-  return response.send(`Hello ${name}!`);
-}
+export default (req, res) => {
+  res.status(200).json({ message: 'Hello, world!' });
+};
