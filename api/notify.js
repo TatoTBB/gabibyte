383599fe0910daa26215ae767bb8c4d2cd7dfa19
TatoTBB <tatohd93@gmail.com>
2024-06-07 06:16:26 -0400

    Updatesito...

---
+++ api/notify.js
@@ -0,0 +1,27 @@
+const nodemailer = require("nodemailer");
+
+export default async (req, res) => {
+  const { email } = req.body;
+
+  let transporter = nodemailer.createTransport({
+    service: "Gmail",
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  let mailOptions = {
+    from: process.env.EMAIL_USER,
+    to: process.env.EMAIL_USER,
+    subject: "New Notification",
+    text: `A new user has signed up with the email: ${email}`,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    res.status(200).json({ message: "Email sent successfully!" });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to send email" });
+  }
+};
